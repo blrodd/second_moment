551c7baeabcaf1a7af66770ff6c4e5a36a670ffb
Rebecca Rodd <rrodd@ucsd.edu>
2018-07-18 16:48:04 -0700

    Use radians instead of degrees

--- matlab/Cart2AZ.m
+++ matlab/Cart2AZ.m
@@ -1,17 +1,17 @@
 function cart0 = Cart2AZ(cart)
     % Convert from cartesian to azimuth
-    cart0 = reorient(cart)
+    cart0 = reorient(cart);
     
-    ndx1 = cart >= 0. & cart < 90.
-    cart0(ndx1) = 90. - cart(ndx1)    
-    ndx2 = cart >= 90. & cart < 180.
-    cart0(ndx2) = 270. - cart(ndx2)
-    ndx3 = cart >= 180. & cart < 360.
-    cart0(ndx3) = 360. - cart(ndx3) + 90.
+    ndx1 = cart >= 0. & cart < pi/2;
+    cart0(ndx1) = pi/2 - cart(ndx1);   
+    ndx2 = cart >= pi/2 & cart < pi;
+    cart0(ndx2) = 3*pi/2 - cart(ndx2);
+    ndx3 = cart >= pi & cart < 2*pi;
+    cart0(ndx3) = 2*pi - cart(ndx3) + pi/2;
 end
 
 function arg = reorient(arg)
     ndx = arg < 0.0;
-    arg(ndx) = arg(ndx) + 360.
+    arg(ndx) = arg(ndx) + 2*pi;
 end
 
